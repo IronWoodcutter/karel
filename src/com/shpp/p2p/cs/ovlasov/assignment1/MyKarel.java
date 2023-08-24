fbac97e1bca485f3f1768c09bd694e108a4ddb0e
IronWoodcutter <a.v.vlasov@ukr.net>
2023-08-24 15:56:23 +0300

    Initial commit

---
+++ src/com/shpp/p2p/cs/ovlasov/assignment1/MyKarel.java
@@ -0,0 +1,13 @@
+package com.shpp.p2p.cs.ovlasov.assignment1;
+
+import com.shpp.karel.KarelTheRobot;
+
+public class MyKarel extends KarelTheRobot {
+
+    /* це коментар :) */
+    public void run() throws Exception {
+        move();
+        pause();
+        turnLeft();
+    }
+}
